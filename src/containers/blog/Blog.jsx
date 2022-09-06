0b8b6d5e7b5c518d01ec9acfffdf4764c2dde5a7
Ed Melech <mr.melech@gmail.com>
2022-09-06 15:47:41 +0100

    fix layout of divs in blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import './blog.css';
 import { Article } from '../../components/'
 import { blog01, blog02, blog03, blog04, blog05 } from './imports'
+import './blog.css';
 
 const Blog = () => (
     <div className='gpt3__blog section__padding' id="blog">
       <div className='gpt3__blog-heading'>
-        <h1 className='gradient__text'>
-          A lot is happening, We are blogging about it. 
-        </h1>
+        <h1 className='gradient__text'>A lot is happening,<br /> We are blogging about it.</h1>
       </div>
       <div className='gpt3__blog-container'>
-        <Article imgUrl={blog01} />
-      </div>
-      <div className='gpt3__blog-container_groupB'>
-          <Article imgUrl={blog02} date='1st Sep, 2022' title="GPT3 and Open AI is the future. Let's explore it. " />
-          <Article imgUrl={blog03} date='1st Sep, 2022' title="GPT3 and Open AI is the future. Let's explore it. "/>
-          <Article imgUrl={blog04} date='1st Sep, 2022' title="GPT3 and Open AI is the future. Let's explore it. "/>
-          <Article imgUrl={blog05} date='1st Sep, 2022' title="GPT3 and Open AI is the future. Let's explore it. "/>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date="sep 6, 2022" text="GPT-3 and Open AI is the future. Let's explore it." />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+            <Article imgUrl={blog02} date='1st Sep, 2022' title="GPT3 and Open AI is the future. Let's explore it. " />
+            <Article imgUrl={blog03} date='1st Sep, 2022' title="GPT3 and Open AI is the future. Let's explore it. "/>
+            <Article imgUrl={blog04} date='1st Sep, 2022' title="GPT3 and Open AI is the future. Let's explore it. "/>
+            <Article imgUrl={blog05} date='1st Sep, 2022' title="GPT3 and Open AI is the future. Let's explore it. "/>
+        </div>
       </div>
     </div>
   );
