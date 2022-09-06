66b344ef7bafd1ae689cc457fc3092ecaa9fbfc6
Ed Melech <mr.melech@gmail.com>
2022-09-06 16:49:57 +0100

    implement footer with styling

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step into the future before others?</h1>
+      </div>
+
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt="logo" />
+          <p>Some random text here</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get In Touch</h4>
+          <p>12 James Bond St London </p>
+          <p>007-123456</p>
+          <p>info@gpt3.com</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>Copyright 2022 GPT-3. All rights reserved</p>
+      </div>
+    </div>
   )
 }
 
